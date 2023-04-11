634c3ed5207fe126399a39ef49443aaa9e8ec6d1
STUzhuo <58807423+STUzhuo@users.noreply.github.com>
2023-04-11 20:05:15 +0800

    Add files via upload

--- ActivityDrivenModel.py
+++ ActivityDrivenModel.py
@@ -1,12 +1,12 @@
 import numpy as np
 import random
-import math
 import matplotlib.pyplot as plt
-import networkx as nx # 导入networkx包
+import networkx as nx
 from pylab import mpl
+import csv
 mpl.rcParams["font.sans-serif"] = ["SimHei"]
 # 定义常量
-N = 1000 # 网络中节点个数
+N = 3000# 网络中节点个数
 m = 3 # 每时刻活跃节点发边数量
 gama = 2.2 # 定义活跃度幂律分布指数
 lBound = 0.001 # 定义活跃度下限
@@ -16,7 +16,7 @@ step = 2000 # 稳态终止时间
 eta = 1
 infectSeeds = 0.01 # 初始I态节点比例
 miuRecover = 0.01 # 定义恢复率，常数,
-lanbtaInter = 0.05
+lanbtaInter = 0.05# 定义
 lanbtaInfect0 = 0.055 # 设置初始的感染值
 LanbtaMax = 1.5 # 最大感染概率
 
@@ -29,7 +29,7 @@ class ActivityDrivenModel:
     def generateActive(self):
         for i in range(N):
             activeValue = (uBound ** (1 - gama) - lBound ** (1 - gama)) * random.random() + lBound ** (1 - gama)
-            activeValue = activeValue ** (1 / (1 - gama))#根据幂律分布生成活跃度值，并创建节点对象存入图中，同时为每个节点添加活跃度和状态属性。状态属性用于表示节点在流行病过程中的感染情况，初始为 S 表示易感。
+            activeValue = activeValue ** (1 / (1 - gama))#
             self.G.add_node(i) # 添加节点到图中
             self.G.nodes[i]['active'] = activeValue # 设置节点的活跃度属性
             self.G.nodes[i]['state'] = 'S' # 设置节点的状态属性
@@ -63,6 +63,9 @@ class ActivityDrivenModel:
 
         # 根据SIS模型进行流行病传播，并返回每个时刻的感染比例列表和恢复比例列表
     def SIS(self, adjMatrixList):
+        global recoverCount
+        infectCountList = []  #每个时刻的感染人数列表
+        recoverCountList = [] #每个时刻的恢复人数列表
         infectRatioList = []  # 存储每个时刻的感染比例的列表
         recoverRatioList = []  # 存储每个时刻的恢复比例的列表
 
@@ -89,15 +92,20 @@ class ActivityDrivenModel:
                             if random.random() < lanbtaInfect: # 如果随机数小于感染率
                                 self.G.nodes[i]['state'] = 'I' # 将节点状态设置为I
                                 break # 跳出循环
+            infectCountList.append(infectCount)
             infectRatio = infectCount / N # 计算感染比例
             infectRatioList.append(infectRatio) # 将感染比例加入到列表中
+
+            recoverCountList.append(recoverCount)
             recoverRatio = recoverCount / N # 计算恢复比例
             recoverRatioList.append(recoverRatio) # 将恢复比例加入到列表中
 
-        return infectRatioList, recoverRatioList
+        return infectRatioList, recoverRatioList, infectCountList, recoverCountList
 
     # 根据SIR模型进行流行病传播，并返回每个时刻的感染比例列表和恢复比例列表
     def SIR(self, adjMatrixList):
+        infectCountList = []  # 每个时刻的感染人数列表
+        recoverCountList = []  # 每个时刻的恢复人数列表
         infectRatioList = [] # 存储每个时刻的感染比例的列表
         recoverRatioList = [] # 存储每个时刻的恢复比例的列表
 
@@ -122,17 +130,22 @@ class ActivityDrivenModel:
                             if random.random() < lanbtaInfect: # 如果随机数小于感染率
                                 self.G.nodes[i]['state'] = 'I' # 将节点状态设置为I
                                 break # 跳出循环
-            infectRatio = infectCount / N # 计算感染比例
-            infectRatioList.append(infectRatio) # 将感染比例加入到列表中
-            recoverRatio = recoverCount / N # 计算恢复比例
-            recoverRatioList.append(recoverRatio) # 将恢复比例加入到列表中
+            infectCountList.append(infectCount)
+            infectRatio = infectCount / N  # 计算感染比例
+            infectRatioList.append(infectRatio)  # 将感染比例加入到列表中
 
-        return infectRatioList, recoverRatioList
+            recoverCountList.append(recoverCount)
+            recoverRatio = recoverCount / N  # 计算恢复比例
+            recoverRatioList.append(recoverRatio)  # 将恢复比例加入到列表中
+
+        return infectRatioList, recoverRatioList,infectCountList,recoverCountList
 
     # 根据SEIR模型进行流行病传播，并返回每个时刻的感染比例列表和恢复比例列表
     def SEIR(self, adjMatrixList):
-        infectRatioList = [] # 存储每个时刻的感染比例的列表
-        recoverRatioList = [] # 存储每个时刻的恢复比例的列表
+        infectCountList = []  # 每个时刻的感染人数列表
+        recoverCountList = []  # 每个时刻的恢复人数列表
+        infectRatioList = []  # 存储每个时刻的感染比例的列表
+        recoverRatioList = []  # 存储每个时刻的恢复比例的列表
 
         infectSeedList = random.sample(range(N), int(N * infectSeeds)) # 从所有节点中随机选择一定比例作为初始感染种子节点
         for i in infectSeedList: # 遍历每个感染种子节点
@@ -158,12 +171,15 @@ class ActivityDrivenModel:
                             if random.random() < lanbtaInfect: # 如果随机数小于感染率
                                 self.G.nodes[i]['state'] = 'E' # 将节点状态设置为E
                                 break # 跳出循环
-            infectRatio = infectCount / N # 计算感染比例
-            infectRatioList.append(infectRatio) # 将感染比例加入到列表中
-            recoverRatio = recoverCount / N # 计算恢复比例
-            recoverRatioList.append(recoverRatio) # 将恢复比例加入到列表中
+            infectCountList.append(infectCount)
+            infectRatio = infectCount / N  # 计算感染比例
+            infectRatioList.append(infectRatio)  # 将感染比例加入到列表中
 
-        return infectRatioList, recoverRatioList
+            recoverCountList.append(recoverCount)
+            recoverRatio = recoverCount / N  # 计算恢复比例
+            recoverRatioList.append(recoverRatio)  # 将恢复比例加入到列表中
+
+        return infectRatioList, recoverRatioList,infectCountList,recoverCountList
 
     # 绘制平均度随时间变化的折线图
     def plotAveDegree(self, aveDegreeList):
@@ -184,12 +200,29 @@ class ActivityDrivenModel:
         plt.title("感染率和恢复率与时间的关系") # 设置标题
         plt.legend() # 显示图例
         plt.show() # 显示图形
+    def plotinfectCount(self,infectCount):
+        plt.figure()
+        plt.plot(range(step),infectCount,label='感染数')
+        plt.xlabel("时间")
+        plt.ylabel("感染人数随时间的变化")
+        plt.legend()
+        plt.show()
+    def plotrecoverCount(self,recoverCount):
+        plt.figure()
+        plt.plot(range(step),recoverCount,label='感染数')
+        plt.xlabel("时间")
+        plt.ylabel("恢复人数随时间的变化")
+        plt.legend()
+        plt.show()
+
 
 # 主函数，测试代码功能
 if __name__ == "__main__":
     model = ActivityDrivenModel() # 创建活跃度驱动模型对象
     model.generateActive() # 生成活跃度值并创建节点对象
     adjMatrixList, aveDegreeList = model.generateRandomNetwork() # 构建时序网络并返回邻接矩阵列表和平均度列表
-    infectRatioList, recoverRatioList = model.SIS(adjMatrixList) # 根据SIS模型进行流行病传播并返回感染比例列表和恢复比例列表
+    infectRatioList, recoverRatioList,infectCount,recoverCount = model.SEIR(adjMatrixList) # 根据SIS模型进行流行病传播并返回感染比例列表和恢复比例列表
     model.plotAveDegree(aveDegreeList) # 绘制平均度随时间变化的折线图
     model.plotInfectRecover(infectRatioList, recoverRatioList) # 绘制感染比例和恢复比例随时间变化的折线图
+    model.plotinfectCount(infectCount)
+    model.plotrecoverCount(recoverCount)
